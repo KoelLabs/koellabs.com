9ee6fbfdd354bd3c80a2ce374a225c7aea5bf6ff
Alexander Metzger <alexander.le@outlook.dk>
2024-09-02 02:09:40 +0200

    prettier auto formatting for consistent style

--- app/page.js
+++ app/page.js
@@ -1,18 +1,16 @@
-import Hero from "@/components/sections/1 - Hero";
-import Logos from "@/components/sections/2 - Logo";
-import Header from "@/components/ui/1 - header";
-import Image from "next/image";
+import Hero from '@/components/sections/1 - Hero';
+import Logos from '@/components/sections/2 - Logo';
+import Header from '@/components/ui/1 - header';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="flex-col flex w-screen relative scroll-smooth">  
-        <div className='z-10 sticky top-0 mx-auto w-full'>
-          <Header />
+    return (
+        <div className="flex-col flex w-screen relative scroll-smooth">
+            <div className="z-10 sticky top-0 mx-auto w-full">
+                <Header />
+            </div>
+            <Hero />
+            <Logos />
         </div>
-      <Hero />
-      <Logos />
-
-
-    </div>
-  );
+    );
 }
