6da380fafc214ef4bb1dde2e4db1a9948b8bb197
Alexander Metzger <alexander.le@outlook.dk>
2025-07-26 12:08:41 +0200

    remove trusted urls

--- lib/auth.ts
+++ lib/auth.ts
@@ -17,7 +17,6 @@ import * as schema from '@/db/schema';
 // }
 
 export const auth = betterAuth({
-  trustedOrigins: ['http://localhost:3000', 'http://192.168.0.12:3000'],
   database: drizzleAdapter(db, {
     provider: 'pg',
     usePlural: true,
