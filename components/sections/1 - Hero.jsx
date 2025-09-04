61791621ca87fba8221f0b1a1423508714f96dc9
arunasrivastava <aruna-sri@outlook.com>
2025-09-03 20:41:43 -0700

    research preview min changes (without venv)

--- components/sections/1 - Hero.jsx
+++ components/sections/1 - Hero.jsx
@@ -15,38 +15,16 @@ export default function Hero() {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
           <section aria-labelledby="hero-heading" className="text-center relative">
             <div className="flex flex-row justify-center items-center my-6">
-              <p className="font-[400] text-md text-neutral-700">Backed by</p>
-              <div className="h-6 bg-black w-6 rounded-md overflow-hidden ml-2 mr-1.5 p-0.5">
-                <svg
-                  aria-label="Mozilla Logo"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0"
-                  y="0"
-                  enableBackground="new 0 0 68 68"
-                  version="1.1"
-                  viewBox="0 0 68 68"
-                  xmlSpace="preserve"
-                >
-                  {' '}
-                  <path d="M0 0H68V68H0z"></path>{' '}
-                  <path
-                    d="M57.9 42.6h3.9V49H49.5V32.4c0-5.1-1.7-7-5-7-4 0-5.6 2.9-5.6 6.9v10.2h3.9v6.4H30.5V32.4c0-5.1-1.7-7-5-7-4 0-5.6 2.9-5.6 6.9v10.2h5.6v6.4h-18v-6.4h3.9V26H7.5v-6.4h12.3V24c1.8-3.1 4.8-5 8.9-5 4.2 0 8.1 2 9.5 6.3 1.6-3.9 4.9-6.3 9.5-6.3 5.3 0 10.1 3.2 10.1 10.1v13.5z"
-                    className="st0 text-white fill-white bg-white"
-                  ></path>{' '}
-                </svg>{' '}
-              </div>
-              <p className="font-[400] text-md text-neutral-700">Mozilla</p>
             </div>
 
             <h1
               id="hero-heading"
               className="text-2xl sm:text-4xl font-bold tracking-[-0.04em] sm:max-w-[500px] lg:max-w-[1400px] mx-auto text-pretty text-gray-900 lg:text-6xl overflow-visible"
             >
-              Master Pronunciation with
+              Speech Technology <br></br>
               <span className="bg-clip-text text-transparent bg-gradient-to-br mr-1 from-sky-600 to-blue-950 overflow-visible py-1">
                 {' '}
-                Classic Movie <span className="pr-0.5">Scenes</span>{' '}
+                for anyone built by  <span className="pr-0.5">everyone</span>{' '}
               </span>
             </h1>
             <svg
@@ -107,8 +85,7 @@ export default function Hero() {
               </defs>
             </svg>
             <p className="mt-6 sm:text-lg leading-8 text-gray-600 max-w-xl mx-auto text-pretty">
-              Practice, perfect, and pronounce like a native language speaker with fun, immersive
-              movie and show clips at your fingertips.
+              An Open Source Approach to Inclusive Innovation
             </p>
             <div className="mt-4 flex flex-col gap-2 sm:flex-row items-center justify-center gap-x-4 w-full">
               <Link
@@ -117,7 +94,7 @@ export default function Hero() {
                 aria-label="Join the waitlist"
               >
                 <Button className="w-full rounded-lg sm:w-40 bg-gradient-to-b border border-double outline-white/50 outline outline-[0.1px] outline-offset-[-2px] border-black from-sky-900 to-blue-950">
-                  Join the Waitlist
+                  Preview Waitlist
                 </Button>
               </Link>
               <Link
